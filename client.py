ae2c4cdabe7df84deab5129d28a87589735fd2dc
httpsxiao <day_clear@163.com>
2025-04-06 20:15:25 +0800

    feat: update

--- client.py
+++ client.py
@@ -20,49 +20,76 @@ load_dotenv()
 
 class MCPClient:
     def __init__(self):
-        """初始化 MCP 客户端"""
+        """
+        初始化 MCP 客户端
+        """
+        # 上下文管理器
         self.exit_stack = AsyncExitStack()
-        self.openai_api_key = os.getenv("OPENAI_API_KEY")  # 读取 OpenAI API Key
-        self.base_url = os.getenv("BASE_URL")  # 读取 BASE YRL
-        self.model = os.getenv("MODEL")  # 读取 model
+
+        # 读取 BASE URL
+        self.base_url = os.getenv("BASE_URL")
+
+        # 读取大模型 API Key
+        self.openai_api_key = os.getenv("OPENAI_API_KEY")
+
+        # 读取大模型
+        self.model = os.getenv("MODEL")
+
         if not self.openai_api_key:
             raise ValueError(
                 "❌ 未找到 OpenAI API Key，请在 .env 文件中设置OPENAI_API_KEY"
             )
+
+        # 初始化大模型
         self.client = OpenAI(api_key=self.openai_api_key, base_url=self.base_url)
+
+        # 与 MCP Server 会话 session
         self.session: Optional[ClientSession] = None
 
     async def connect_to_server(self, server_script_path: str):
-        """连接到MCP服务器并列出可用工具"""
+        """
+        连接到MCP服务器并列出可用工具
+        """
+
+        # Server 必须是 python 或者 js
         is_python = server_script_path.endswith(".py")
         is_js = server_script_path.endswith(".js")
         if not (is_python or is_js):
             raise ValueError("服务器脚本必须是.py或.js文件")
+
+        # 拼接 MCP 服务器参数
         command = "python" if is_python else "node"
         server_params = StdioServerParameters(
-            command=command, args=[server_script_path], env=None
+            command=command, args=[server_script_path]
         )
+
         # 启动 MCP 服务器并建立通信
         stdio_transport = await self.exit_stack.enter_async_context(
             stdio_client(server_params)
         )
         self.stdio, self.write = stdio_transport
+
+        # MCP 会话管理
         self.session = await self.exit_stack.enter_async_context(
             ClientSession(self.stdio, self.write)
         )
         await self.session.initialize()
 
+        # 列出可用的 MCP 工具
         response = await self.session.list_tools()
         tools = response.tools
         print("\n已连接到服务器,支持以下工具:", [tool.name for tool in tools])
 
     async def process_query(self, query: str) -> str:
-        """使用大模型处理查询并调用可用的 MCP 工具"""
+        """
+        使用大模型处理查询并调用可用的 MCP 工具
+        """
+
+        # 将用户输入 + 可用的工具给到大模型
         messages = [
             {"role": "user", "content": query},
         ]
-        response = await self.session.list_tools()
-
+        toolList = await self.session.list_tools()
         available_tools = [
             {
                 "type": "function",
@@ -72,22 +99,21 @@ class MCPClient:
                     "input_schema": tool.inputSchema,
                 },
             }
-            for tool in response.tools
+            for tool in toolList.tools
         ]
 
-        print("\n available_tools 可用工具:", available_tools)
-
+        # 上述内容作为参数开启大模型会话
         response = self.client.chat.completions.create(
             model=self.model,
             messages=messages,
             tools=available_tools,
         )
 
-        # 处理返回的内容
+        # 获得大模型返回的内容
         modelResult = response.choices[0]
 
         if modelResult.finish_reason == "tool_calls":
-            # 如何是需要使用工具，就解析工具
+            # 如何是需要使用工具，就解析出来需要的 工具 + 参数
             tool_call = modelResult.message.tool_calls[0]
             tool_name = tool_call.function.name
             tool_args = json.loads(tool_call.function.arguments)
@@ -98,7 +124,7 @@ class MCPClient:
                 f"\n\n[Callingtool {tool_name} with args {tool_args}]\n toolResult is {toolResult.content[0].text}\n"
             )
 
-            # 将模型返回的调用哪个工具数据和工具执行完成后的数据都存入messages中
+            # 将模型返回的调用哪个工具数据和工具执行完成后的数据都存入 messages 中，以 tool 身份发送
             messages.append(modelResult.message.model_dump())
             messages.append(
                 {
@@ -108,30 +134,44 @@ class MCPClient:
                 }
             )
 
-            # 将上面的结果再返回给大模型用于生产最终的结果
+            # 将工具的结果再返回给大模型用于生产最终的结果
             response = self.client.chat.completions.create(
                 model=self.model,
                 messages=messages,
             )
+
+            # 过滤推理模型的推理部分，也可以在上一步设置 max_tokens 参数，但是文字可能会被截断
             rmThinkContent = re.sub(
                 r"<think>.*?</think>",
                 "",
                 response.choices[0].message.content,
                 flags=re.DOTALL,
             )
+
+            # 最终大模型二次的返回结果给到用户
             return rmThinkContent
 
         return modelResult.message.content
 
     async def chat_loop(self):
-        """运行交互式聊天循环"""
+        """
+        运行交互式聊天循环
+        """
+        # 给用户的提示
         print("\nMCP 客户端已启动！输入 'quit' 退出")
+
         while True:
             try:
                 query = input("\n请输入内容:").strip()
+
+                # 如果输入 quit，则停止运行
                 if query.lower() == "quit":
                     break
-                response = await self.process_query(query)  # 发送用户输入到大模型
+
+                # 用户的输入给到大模型
+                response = await self.process_query(query)
+
+                # 输出大模型的结果
                 print(f"\n🤖 大模型: {response}")
             except Exception as e:
                 print(f"\n⚠️ 发生错误: {str(e)}")
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import httpx
 import random
@@ -9,12 +10,14 @@ mcp = FastMCP("WeatherServer")
 random_array = ["晴", "阴", "雨", "雪", "雾", "雷", "风", "沙", "尘", "霾"]
 
 
-async def fetch_weather(city: str) -> str | None:
+async def mock_fetch_weather(city: str) -> str | None:
     """
     获取指定城市的天气信息
     """
     temperature = random.randrange(5, 30)
     humidity = random.randrange(30, 70)
+
+    await asyncio.sleep(1)
 
     return f"🏙 {city} 的天气：🌡️ {temperature}℃，💧 {humidity}%，🌬️ {random.choice(random_array)}"
 
@@ -26,8 +29,8 @@ async def query_weather(city: str) -> str:
     :param city: 城市名称（需使用英文）
     :return: 天气信息
     """
-    result = await fetch_weather(city)
-    print("wokao", result)
+    result = await mock_fetch_weather(city)
+
     return result
 
 
